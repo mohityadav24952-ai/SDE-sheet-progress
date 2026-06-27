e54670ef9af99d5747d6038bee0e7ac5a6bde34b
Mohit Kumar Yadav <mohityadav24952@gmail.com>
2026-06-27 17:44:44 +0530

    Time: 0 ms (100%), Space: 15.9 MB (82.63%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+
+        // Step 1: Find total sum of the array
+        int totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        // Left side sum initially 0
+        int leftSum = 0;
+
+        // Traverse the array
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Right sum = Total Sum - Left Sum - Current Element
+            int rightSum = totalSum - leftSum - nums[i];
+
+            // If both sums are equal, current index is the pivot
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            // Include current element in left sum
+            leftSum += nums[i];
+        }
+
+        // No pivot index found
+        return -1;
+    }
+
+    int findMiddleIndex(vector<int>& nums) {
+        return pivotIndex(nums);
+    }
+};
